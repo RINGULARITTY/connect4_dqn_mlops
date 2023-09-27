ad08913d532ab8053169e94ee34cffe438f0cef7
RINGULARITTY <63746619+RINGULARITTY@users.noreply.github.com>
2023-09-27 19:30:31 +0200

    Change files

--- src/client.py
+++ src/client.py
@@ -1,38 +1,30 @@
 import streamlit as st
-import requests as rq
 
-url = ...
-
-st.title("Connect 4")
-
-# Initier la grille de jeu
 ROWS, COLS = 6, 7
 grid = [[0 for _ in range(COLS)] for _ in range(ROWS)]
 
 def display_grid():
-    st.write("Connect 4")
-    
-    # Afficher les boutons pour jouer un pion
-    col_selected = None
-    for col in range(COLS):
-        if st.button(f"Play in column {col}"):
-            col_selected = col
-    st.write("Selected column:", col_selected)
+    st.title('Connect 4')
 
-    # Mettre à jour la grille (c'est ici que vous ajouteriez la logique du jeu)
-    
-
-    # Afficher la grille
     for row in grid:
         row_display = []
         for cell in row:
             if cell == 0:
-                row_display.append("⚪")  # Cellule vide
+                row_display.append("⚪")
             elif cell == 1:
-                row_display.append("🔴")  # Pion rouge
+                row_display.append("🔴")
             else:
-                row_display.append("🟡")  # Pion jaune
-        st.write(" ".join(row_display))
+                row_display.append("🟡")
+        
+        row_str = ' '.join(row_display)
+        st.markdown(f"<span style='font-size: 60px'>{row_str}</span>", unsafe_allow_html=True)
+    
+    button_html = ''.join(f'<button class="grid-button" id="button{col}" style="width: 85px; height: 50px; margin-right: 35px">{col}</button>' for col in range(COLS))
 
-if __name__ == "__main__":
+    st.markdown(
+        f'<div style="display: flex; justify-content: start; width: {85*COLS}px;">{button_html}</div>',
+        unsafe_allow_html=True
+    )
+
+if __name__ == '__main__':
     display_grid()
